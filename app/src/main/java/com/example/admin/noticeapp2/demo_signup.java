9f7c8f68bf5326eee475aaea9f8f21b66b4a63a5
vikrant <vikrantg.1626@gmail.com>
2019-03-11 10:11:08 +0530

    new signup

---
+++ app/src/main/java/com/example/admin/noticeapp2/demo_signup.java
@@ -0,0 +1,13 @@
+package com.example.admin.noticeapp2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class demo_signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo_signup);
+    }
+}
